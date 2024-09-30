58b7e3046d185571a5e135971e1096d7eede3eb4
ReturDev <sergiomm50@gmail.com>
2024-10-01 00:04:38 +0200

    feat: Create EquipmentEntity with validation
    - Added EquipmentEntity class to represent equipment in the database.
    - Defined unique constraint for name field to prevent duplicates.
    - Configured @Relation annotation for HATEOAS support.
    - Added necessary validations for fields.

---
+++ src/main/java/com/returdev/gym_exercises_api/entities/EquipmentEntity.java
@@ -0,0 +1,33 @@
+package com.returdev.gym_exercises_api.entities;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.hateoas.server.core.Relation;
+
+@Entity
+@Table(
+        name = "equipments"
+)
+@Relation(collectionRelation = "equipments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EquipmentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "equipment_sequence", allocationSize = 1)
+    @Column(name = "equipment_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 25)
+    @NotBlank
+    @Length(min = 3, max = 25)
+    private String name;
+
+}
