525afd313151ae25b46160f4e1e675bf8433b0d7
ReturDev <sergiomm50@gmail.com>
2024-09-30 23:26:10 +0200

    Initial commit

---
+++ src/main/java/com/returdev/gym_exercises_api/GymExercisesApiApplication.java
@@ -0,0 +1,13 @@
+package com.returdev.gym_exercises_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymExercisesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymExercisesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/returdev/gym_exercises_api/GymExercisesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.returdev.gym_exercises_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymExercisesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
