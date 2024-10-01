6dab0d648765bfcd67be58819450911840863cdd
ReturDev <sergiomm50@gmail.com>
2024-10-01 23:31:40 +0200

    feat: Create ExerciseRepository with custom queries for muscle and equipment filtering
    - Added repository interface for ExerciseEntity management.
    - Implemented queries to find exercises by muscle, muscle engagement ID, and equipment ID.
    - Added query to fetch exercises based on multiple muscle engagements.
    - Added existence check for unique name and equipment combination to enforce business rules.

---
+++ src/main/java/com/returdev/gym_exercises_api/repositories/ExerciseRepository.java
@@ -0,0 +1,35 @@
+package com.returdev.gym_exercises_api.repositories;
+
+
+import com.returdev.gym_exercises_api.entities.ExerciseEntity;
+import com.returdev.gym_exercises_api.enums.Muscle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<ExerciseEntity, Long> {
+
+    @Query("SELECT ex FROM ExerciseEntity ex JOIN ex.musclesEngagement me WHERE me.muscle = :muscle")
+    Page<ExerciseEntity> findByMuscle(@Param("muscle") Muscle muscle, Pageable pageable);
+
+    @Query("SELECT ex FROM ExerciseEntity ex JOIN ex.musclesEngagement me WHERE me.muscle IN :muscles")
+    Page<ExerciseEntity> findByMuscles(@Param("muscles") List<Muscle> muscles, Pageable pageable);
+
+    @Query("SELECT ex FROM ExerciseEntity ex JOIN ex.musclesEngagement me WHERE me.id = :id")
+    Page<ExerciseEntity> findByMuscleEngagementId(@Param("id") Long id, Pageable pageable);
+
+    @Query("SELECT ex FROM ExerciseEntity ex JOIN ex.musclesEngagement me WHERE me.id IN :ids")
+    Page<ExerciseEntity> findByMuscleEngagementIds(@Param("ids") List<Long> ids, Pageable pageable);
+
+    Page<ExerciseEntity> findByEquipmentId(Long id, Pageable pageable);
+
+    boolean existsByNameAndEquipmentId(String name, Long equipmentId);
+
+
+}
