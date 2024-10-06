95b321e76278e1e3c65f1829e47003aaa6c0ac84
ReturDev <sergiomm50@gmail.com>
2024-10-06 12:21:06 +0200

    feat: define ExerciseService interface for exercise management
    - Added methods for retrieving exercises with pagination, getting exercise by ID, and saving, updating, and partially updating exercises.
    - Included method for save-or-update functionality and exercise deletion.
    - Established a contract for managing exercise entities.

---
+++ src/main/java/com/returdev/gym_exercises_api/service/ExerciseService.java
@@ -0,0 +1,25 @@
+package com.returdev.gym_exercises_api.service;
+
+import com.returdev.gym_exercises_api.entities.ExerciseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface ExerciseService {
+
+    Page<ExerciseEntity> getAllExercises(Pageable pageable);
+
+    Optional<ExerciseEntity> getExerciseById(Long id);
+
+    ExerciseEntity saveExercise(ExerciseEntity exercise);
+
+    ExerciseEntity updateExercise(ExerciseEntity exercise);
+
+    ExerciseEntity partialUpdateExercise(ExerciseEntity patchExercise);
+
+    ExerciseEntity saveOrUpdateExercise(ExerciseEntity exercise);
+
+    void deleteExercise(Long id);
+
+}
