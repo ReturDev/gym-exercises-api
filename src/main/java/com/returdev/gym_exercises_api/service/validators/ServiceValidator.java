946b8fcf1b8fac973c8c7a9c7205cd6b362193fc
ReturDev <sergiomm50@gmail.com>
2024-10-09 22:36:09 +0200

    feat: create ServiceValidator interface for entity validation
    - Added new methods for validating the retrieval, saving, updating, and deletion of entities (Exercise, Equipment).
    - Introduced methods for specific validations like validateGetExerciseById and validateGetEquipmentById.

---
+++ src/main/java/com/returdev/gym_exercises_api/service/validators/ServiceValidator.java
@@ -0,0 +1,61 @@
+package com.returdev.gym_exercises_api.service.validators;
+
+import com.returdev.gym_exercises_api.entities.EquipmentEntity;
+import com.returdev.gym_exercises_api.entities.ExerciseEntity;
+import com.returdev.gym_exercises_api.entities.MuscleEngagementEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public interface ServiceValidator {
+
+    void validateGetExerciseById(Long id);
+
+    void validateGetEquipmentById(Long id);
+
+    void validateSaveEquipment(
+            EquipmentEntity equipment,
+            Predicate<String> existsByName
+    );
+
+    void validateUpdateEquipment(
+            EquipmentEntity equipment,
+            Predicate<Long> existsById,
+            Predicate<String> existsByName
+    );
+
+    void validateDeleteEquipment(
+            Long id,
+            Predicate<Long> existsById
+    );
+
+    ExerciseEntity validateSaveExercise(
+            ExerciseEntity exercise,
+            BiPredicate<String, Long> existsByNameAndEquipmentId,
+            Function<Long, Optional<EquipmentEntity>> getEquipmentById,
+            Function<List<MuscleEngagementEntity>, List<MuscleEngagementEntity>> getMuscleEngagementsWithId
+    );
+
+
+    ExerciseEntity validateUpdateExercise(
+            ExerciseEntity exercise,
+            Predicate<Long> existsById,
+            BiPredicate<String, Long> existsByNameAndEquipmentId,
+            Function<Long, Optional<EquipmentEntity>> getEquipmentById,
+            Function<List<MuscleEngagementEntity>, List<MuscleEngagementEntity>> getMuscleEngagementsWithId
+    );
+
+    ExerciseEntity validatePartialUpdateExercise(
+            final ExerciseEntity exercise,
+            Function<Long, Optional<ExerciseEntity>> getExerciseById,
+            BiPredicate<String, Long> existsByNameAndEquipmentId,
+            Function<Long, Optional<EquipmentEntity>> getEquipmentById,
+            Function<List<MuscleEngagementEntity>, List<MuscleEngagementEntity>> getMuscleEngagementsWithId
+    );
+
+
+    void validateDeleteExercise(Long id, Predicate<Long> existsById);
+}
