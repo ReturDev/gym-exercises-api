bd774e20d13f3e560cdfafe7a2f21abce9c8ca59
ReturDev <sergiomm50@gmail.com>
2024-10-12 15:25:45 +0200

    feat: add EntityDtoMapper interface for DTO mapping
    - Introduced EntityDtoMapper interface to handle conversions between entities and DTOs.
    - Added methods for mapping Equipment and Exercise entities to their respective response DTOs.
    - Included methods for converting request DTOs to entities and for pagination handling.
    - Supports better separation of concerns in data transformation for API responses.

---
+++ src/main/java/com/returdev/gym_exercises_api/mappers/EntityDtoMapper.java
@@ -0,0 +1,28 @@
+package com.returdev.gym_exercises_api.mappers;
+
+import com.returdev.gym_exercises_api.dto.request.EquipmentRequestDTO;
+import com.returdev.gym_exercises_api.dto.request.ExerciseRequestDTO;
+import com.returdev.gym_exercises_api.dto.request.pagination.PaginationRequestDTO;
+import com.returdev.gym_exercises_api.dto.response.ContentResponseDTO;
+import com.returdev.gym_exercises_api.dto.response.EquipmentResponseDTO;
+import com.returdev.gym_exercises_api.dto.response.ExerciseResponseDTO;
+import com.returdev.gym_exercises_api.entities.EquipmentEntity;
+import com.returdev.gym_exercises_api.entities.ExerciseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface EntityDtoMapper {
+    
+    ContentResponseDTO<List<EquipmentResponseDTO>> equipmentEntityToContentResponse(Page<EquipmentEntity> equipmentEntityPage);
+    ContentResponseDTO<EquipmentResponseDTO> equipmentEntityToContentResponse(EquipmentEntity equipmentEntity);
+    EquipmentEntity equipmentRequestDtoToEntity(EquipmentRequestDTO equipmentRequestDTO);
+
+    ContentResponseDTO<List<ExerciseResponseDTO>> exerciseEntityToContentResponse(Page<ExerciseEntity> exerciseEntityPage);
+    ContentResponseDTO<ExerciseResponseDTO> exerciseEntityToContentResponse(ExerciseEntity exerciseEntity);
+    ExerciseEntity exerciseRequestDtoToEntity(ExerciseRequestDTO exerciseRequestDTO);
+
+    Pageable paginationRequestDtoToPageable(PaginationRequestDTO paginationRequestDTO);
+
+}
