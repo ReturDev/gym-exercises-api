7a1a2ae5b773defa30a570e72756f8953f734c37
ReturDev <sergiomm50@gmail.com>
2024-10-12 15:39:30 +0200

    chore: create ApplicationConfig for application-wide settings
    - Added ApplicationConfig class to centralize application configurations.
    - Defined API version ("v1") as a constant for consistent endpoint management.

---
+++ src/main/java/com/returdev/gym_exercises_api/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.returdev.gym_exercises_api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    public static final String API_VERSION = "v1";
+
+}
