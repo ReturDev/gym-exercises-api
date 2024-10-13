b7ad376e1815ffc95070e9a71a082562ffe11bcd
ReturDev <sergiomm50@gmail.com>
2024-10-13 13:46:09 +0200

    fix: update fromString method in Muscle enum for better error handling
    - Changed the implementation of the fromString method to handle IllegalArgumentException.
    - Added a custom InvalidEnumValueException to provide detailed error messages for invalid enum values.
    - Introduced a validValues method to display the expected enum values in error messages.

--- src/main/java/com/returdev/gym_exercises_api/enums/Muscle.java
+++ src/main/java/com/returdev/gym_exercises_api/enums/Muscle.java
@@ -1,6 +1,13 @@
 package com.returdev.gym_exercises_api.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.returdev.gym_exercises_api.exceptions.InvalidEnumValueException;
+import com.returdev.gym_exercises_api.util.message.MessageManager;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public enum Muscle {
 
@@ -32,7 +39,22 @@ public enum Muscle {
 
     @JsonCreator
     public static Muscle fromString(String value) throws IllegalArgumentException {
-        return Muscle.valueOf(value.toUpperCase());
+        try{
+            return Muscle.valueOf(value.toUpperCase());
+        }catch (IllegalArgumentException ex){
+            throw new InvalidEnumValueException(
+                    value,
+                    "validation.invalid_enum_value.message",
+                    validValues()
+            );
+        }
+    }
+
+    private static String validValues(){
+        List<String> names = Arrays.stream(Muscle.values())
+                .map(Muscle::name)
+                .toList();
+        return String.join(", ", names.subList(0, names.size() - 1)) + " or " + names.get(names.size() - 1);
     }
 
 }
