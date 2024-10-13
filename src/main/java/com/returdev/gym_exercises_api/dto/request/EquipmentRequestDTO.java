435dc79c16123dda946830214c9341050abc8537
ReturDev <sergiomm50@gmail.com>
2024-10-13 12:01:47 +0200

    refactor: improve validation for EquipmentRequestDTO
    - Added custom validation message for @NotBlank using "{validation.not_blank.message}" to enhance flexibility in error messages.
    - Introduced @Size constraint for name with a range of 3 to 25 characters, using custom message "{validation.size.message}" to ensure the input length adheres to defined standards.

--- src/main/java/com/returdev/gym_exercises_api/dto/request/EquipmentRequestDTO.java
+++ src/main/java/com/returdev/gym_exercises_api/dto/request/EquipmentRequestDTO.java
@@ -1,8 +1,11 @@
 package com.returdev.gym_exercises_api.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record EquipmentRequestDTO(
         Long id,
-        @NotBlank String name
+        @NotBlank(message = "{validation.not_blank.message}")
+        @Size(min = 3, max = 25, message = "{validation.size.message}")
+        String name
 ) {}
