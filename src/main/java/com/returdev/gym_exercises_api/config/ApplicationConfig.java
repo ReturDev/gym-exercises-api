cd65769d9a196320d59ab8c770bedcc4735e3a71
ReturDev <sergiomm50@gmail.com>
2024-10-20 14:20:12 +0200

    refactor: enable scheduling in ApplicationConfig
    - Added @EnableScheduling annotation to enable scheduled tasks in the application.
    - Removed API_VERSION constant as it will be deprecated for future changes.

--- src/main/java/com/returdev/gym_exercises_api/config/ApplicationConfig.java
+++ src/main/java/com/returdev/gym_exercises_api/config/ApplicationConfig.java
@@ -1,10 +1,8 @@
 package com.returdev.gym_exercises_api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-public class ApplicationConfig {
-
-    public static final String API_VERSION = "v1";
-
-}
+@EnableScheduling
+public class ApplicationConfig {}
