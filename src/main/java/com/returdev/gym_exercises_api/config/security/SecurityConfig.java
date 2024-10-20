50080e839fe8b87b881c65a982fce763800db7e1
ReturDev <sergiomm50@gmail.com>
2024-10-20 14:52:06 +0200

    feat: implement SecurityConfig for JWT authentication
    - Created `SecurityConfig` class to configure security settings for the application.
    - Enabled web security and method security.
    - Configured a stateless session management policy.
    - Added a custom JWT token filter to handle authentication via JSON Web Tokens.
    - Provided an authentication manager bean for managing authentication processes.

---
+++ src/main/java/com/returdev/gym_exercises_api/config/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.returdev.gym_exercises_api.config.security;
+
+import com.returdev.gym_exercises_api.manager.security.JwtManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtManager jwtManager;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtTokenFilter(jwtManager), BasicAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
