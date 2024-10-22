feaf6a1a2538ac95cda7c50155527bdbf28af5d9
ReturDev <sergiomm50@gmail.com>
2024-10-22 23:43:41 +0200

    docs: add Javadoc comments to MessageManager interface
    - Added documentation in MessageManager interface to clarify its purpose and methods for handling message retrieval and formatting, enhancing code understanding.

--- src/main/java/com/returdev/gym_exercises_api/manager/message/MessageManager.java
+++ src/main/java/com/returdev/gym_exercises_api/manager/message/MessageManager.java
@@ -1,9 +1,31 @@
 package com.returdev.gym_exercises_api.manager.message;
 
+/**
+ * Interface for managing messages and localization in the application.
+ *
+ * <p>
+ * This interface provides methods for retrieving messages from resource bundles,
+ * allowing for internationalization and parameterized messages.
+ * </p>
+ */
 public interface MessageManager {
 
+    /**
+     * Retrieves a message associated with the specified resource key.
+     *
+     * @param resourceMsgKey the key of the message to retrieve
+     * @return the message corresponding to the provided key
+     */
     String getMessage(String resourceMsgKey);
 
+    /**
+     * Retrieves a message associated with the specified resource key and formats it
+     * with the provided parameters.
+     *
+     * @param resourceMsgKey the key of the message to retrieve
+     * @param params         an array of parameters to format the message
+     * @return the formatted message corresponding to the provided key
+     */
     String getMessageWithParams(String resourceMsgKey, Object[] params);
-
 }
+
