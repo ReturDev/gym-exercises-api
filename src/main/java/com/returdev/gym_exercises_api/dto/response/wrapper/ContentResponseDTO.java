8755a4cc926f4fce220b7e5a9c7cee919db1ceb1
ReturDev <sergiomm50@gmail.com>
2024-10-22 23:54:47 +0200

    docs: add Javadoc comments to ContentResponseDTO record
    - Added documentation to ContentResponseDTO record to explain its role in encapsulating paginated content responses for the API.

--- src/main/java/com/returdev/gym_exercises_api/dto/response/wrapper/ContentResponseDTO.java
+++ src/main/java/com/returdev/gym_exercises_api/dto/response/wrapper/ContentResponseDTO.java
@@ -3,6 +3,20 @@ package com.returdev.gym_exercises_api.dto.response.wrapper;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 
+/**
+ * A generic Data Transfer Object (DTO) for wrapping a single content item
+ * in the response.
+ *
+ * <p>
+ * This record is designed to encapsulate any type of content, allowing
+ * for flexible response structures. It can be used to return a single
+ * entity or data object as part of a broader API response.
+ * </p>
+ *
+ * @param <T> The type of content being wrapped.
+ * @param content The content of type {@code T} to be included in the response.
+ */
 public record ContentResponseDTO<T>(
         @JsonProperty("data") T content
 ) {}
+
