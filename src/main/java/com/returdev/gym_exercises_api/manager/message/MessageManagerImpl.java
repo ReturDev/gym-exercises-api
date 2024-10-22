b387125e4c6bfe2b540cb46c3f31d373b19568c2
ReturDev <sergiomm50@gmail.com>
2024-10-22 23:46:18 +0200

    docs: add Javadoc comments to MessageManagerImpl class
    - Added documentation in MessageManagerImpl class to provide clarity on its implementation of message retrieval and formatting, improving code maintainability.

--- src/main/java/com/returdev/gym_exercises_api/manager/message/MessageManagerImpl.java
+++ src/main/java/com/returdev/gym_exercises_api/manager/message/MessageManagerImpl.java
@@ -4,20 +4,41 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
+/**
+ * Implementation of the MessageManager interface for retrieving messages
+ * from resource bundles, supporting internationalization.
+ *
+ * <p>
+ * This class utilizes Spring's MessageSource to fetch messages based on
+ * the provided keys and current locale. It allows for both simple message
+ * retrieval and formatted messages with parameters.
+ * </p>
+ */
 @Component
-public class MessageManagerImpl implements MessageManager{
+public class MessageManagerImpl implements MessageManager {
 
     private final MessageSource messageSource;
 
+    /**
+     * Constructs a MessageManagerImpl with the specified MessageSource.
+     *
+     * @param messageSource the MessageSource to be used for fetching messages
+     */
     public MessageManagerImpl(MessageSource messageSource) {
         this.messageSource = messageSource;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getMessage(String resourceMsgKey) {
         return getMessageWithParams(resourceMsgKey, null);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getMessageWithParams(String resourceMsgKey, Object[] params) {
         return messageSource.getMessage(
@@ -27,3 +48,5 @@ public class MessageManagerImpl implements MessageManager{
         );
     }
 }
+
+
