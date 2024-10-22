30ebdf7ca23f70ab279a1b164688c7c0a75f7a54
ReturDev <sergiomm50@gmail.com>
2024-10-23 00:33:35 +0200

    docs: add Javadoc comments to AuthController class
    - Added documentation to the AuthController class to clarify its role in managing authentication processes, detailing methods for user login, token generation, and handling authentication-related requests.

--- src/main/java/com/returdev/gym_exercises_api/controller/AuthController.java
+++ src/main/java/com/returdev/gym_exercises_api/controller/AuthController.java
@@ -1,9 +1,7 @@
 package com.returdev.gym_exercises_api.controller;
 
 import com.returdev.gym_exercises_api.dto.response.TokenResponseDTO;
-import com.returdev.gym_exercises_api.manager.message.MessageManager;
 import com.returdev.gym_exercises_api.mappers.EntityDtoMapper;
-import com.returdev.gym_exercises_api.model.auth.AuthToken;
 import com.returdev.gym_exercises_api.repositories.security.TokenRepository;
 import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for authentication-related operations.
+ *
+ * <p>
+ * This controller provides endpoints for user authentication, including obtaining a token.
+ * It is accessible to all authenticated users.
+ * </p>
+ */
 @PermitAll
 @RestController
 @RequestMapping("v1/auth")
@@ -20,15 +26,20 @@ public class AuthController {
 
     private final TokenRepository repository;
     private final EntityDtoMapper mapper;
-    private final MessageManager messageManager;
 
+    /**
+     * Retrieves the authentication token for the user.
+     *
+     * @return A response entity containing the token details.
+     */
     @GetMapping()
-    public ResponseEntity<TokenResponseDTO> getUserToken(){
+    public ResponseEntity<TokenResponseDTO> getUserToken() {
+        // Fetch the token from the repository and map it to the response DTO
         return ResponseEntity.ok(
                 mapper.authTokenToResponse(
                         repository.getToken()
                 )
         );
     }
-
 }
+
