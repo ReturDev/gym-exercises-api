674f1c5bed63638449c6ed2a7a902095783260ec
ReturDev <sergiomm50@gmail.com>
2024-10-22 13:53:15 +0200

    refactor: update EquipmentService to enforce validation on equipment parameters
    - Added @Validated annotation to the EquipmentService interface to enable validation.
    - Updated saveEquipment method to require @Valid annotation for the EquipmentEntity parameter, ensuring that validation is applied during saving.
    - Changed updateEquipment method to include @Valid annotation on the EquipmentEntity parameter, enforcing validation during updates.

--- src/main/java/com/returdev/gym_exercises_api/service/data/equipment/EquipmentService.java
+++ src/main/java/com/returdev/gym_exercises_api/service/data/equipment/EquipmentService.java
@@ -1,20 +1,23 @@
 package com.returdev.gym_exercises_api.service.data.equipment;
 
 import com.returdev.gym_exercises_api.model.entities.EquipmentEntity;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Optional;
 
+@Validated
 public interface EquipmentService {
 
     Optional<EquipmentEntity> getEquipmentById(Long id);
 
     Page<EquipmentEntity> getAllEquipments(Pageable pageable);
 
-    EquipmentEntity saveEquipment(EquipmentEntity equipment);
+    EquipmentEntity saveEquipment(@Valid EquipmentEntity equipment);
 
-    EquipmentEntity updateEquipment(EquipmentEntity equipment);
+    EquipmentEntity updateEquipment(@Valid EquipmentEntity equipment);
 
     void deleteEquipment(Long id);
 
