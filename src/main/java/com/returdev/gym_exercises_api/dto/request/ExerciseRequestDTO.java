d5eca27662e51c13ea962796d4a3adc464929382
ReturDev <sergiomm50@gmail.com>
2024-10-23 00:18:25 +0200

    docs: add Javadoc comments to ExerciseRequestDTO record
    - Added documentation to ExerciseRequestDTO record to clarify its purpose in representing requests for exercise data, including the relevant attributes for creating or updating exercises.

--- src/main/java/com/returdev/gym_exercises_api/dto/request/ExerciseRequestDTO.java
+++ src/main/java/com/returdev/gym_exercises_api/dto/request/ExerciseRequestDTO.java
@@ -8,6 +8,23 @@ import jakarta.validation.constraints.Size;
 
 import java.util.Set;
 
+/**
+ * A Data Transfer Object (DTO) representing a request to create or update an exercise.
+ *
+ * <p>
+ * This record is used to encapsulate the necessary information required
+ * for creating or updating an exercise in an API request. It includes
+ * validation annotations to ensure that the provided values adhere to
+ * specific constraints, ensuring data integrity when processing exercise
+ * related requests.
+ * </p>
+ *
+ * @param id The unique identifier of the exercise. Can be null for new exercises.
+ * @param name The name of the exercise. Must not be blank and should be between 10 and 50 characters.
+ * @param description A brief description of the exercise. Defaults to an empty string if null.
+ * @param equipmentId The unique identifier of the equipment associated with the exercise. Must not be null.
+ * @param muscleEngagements A set of muscle engagement details associated with the exercise. Must not be empty.
+ */
 public record ExerciseRequestDTO(
         Long id,
         @NotBlank(message = "{validation.not_blank.message}")
@@ -19,11 +36,21 @@ public record ExerciseRequestDTO(
         @NotEmpty(message = "{validation.not_empty.message}") Set<MuscleEngagementRequestDTO> muscleEngagements
 ) {
 
+    /**
+     * Constructor to initialize an ExerciseRequestDTO.
+     *
+     * @param id The unique identifier of the exercise. Can be null for new exercises.
+     * @param name The name of the exercise. Must not be blank and should be between 10 and 50 characters.
+     * @param description A brief description of the exercise. Defaults to an empty string if null.
+     * @param equipmentId The unique identifier of the equipment associated with the exercise. Must not be null.
+     * @param muscleEngagements A set of muscle engagement details associated with the exercise. Must not be empty.
+     */
     public ExerciseRequestDTO(Long id, String name, String description, Long equipmentId, Set<MuscleEngagementRequestDTO> muscleEngagements) {
         this.id = id;
         this.name = name;
-        this.description = description == null ? "" : description;
+        this.description = description == null ? "" : description; // Ensure description defaults to an empty string if null
         this.equipmentId = equipmentId;
         this.muscleEngagements = muscleEngagements;
     }
 }
+
