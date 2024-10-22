08a94fe0878d6f1e413953bf2396244b08f15760
ReturDev <sergiomm50@gmail.com>
2024-10-23 00:36:43 +0200

    docs: add Javadoc comments to ApplicationConfig class
    - Added documentation to the ApplicationConfig class to describe its purpose in configuring application-specific beans and settings for the Spring context.

--- src/main/java/com/returdev/gym_exercises_api/config/ApplicationConfig.java
+++ src/main/java/com/returdev/gym_exercises_api/config/ApplicationConfig.java
@@ -3,6 +3,15 @@ package com.returdev.gym_exercises_api.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+/**
+ * General application configuration class.
+ *
+ * <p>
+ * This configuration class is used to define beans and other general configurations
+ * for the application.
+ * </p>
+ */
 @Configuration
 @EnableScheduling
 public class ApplicationConfig {}
