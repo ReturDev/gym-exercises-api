00ba4097a85d255ad67b0c180d973a6eae4ba884
ReturDev <sergiomm50@gmail.com>
2024-10-22 23:51:05 +0200

    docs: add Javadoc comments to MuscleEngagementResponseDTO record
    - Added documentation to MuscleEngagementResponseDTO record to clarify its purpose and structure for representing muscle engagement data in responses.

--- src/main/java/com/returdev/gym_exercises_api/dto/response/MuscleEngagementResponseDTO.java
+++ src/main/java/com/returdev/gym_exercises_api/dto/response/MuscleEngagementResponseDTO.java
@@ -1,5 +1,17 @@
 package com.returdev.gym_exercises_api.dto.response;
 
+/**
+ * Data Transfer Object (DTO) for representing muscle engagement details in the response.
+ *
+ * <p>
+ * This record holds the information about a muscle engagement, including its ID,
+ * the muscle involved, and the activation level of that muscle.
+ * </p>
+ *
+ * @param id The unique identifier for the muscle engagement.
+ * @param muscle The name of the muscle involved in the engagement.
+ * @param muscleActivationLevel  The activation level of the muscle (e.g., HIGH, MEDIUM, LOW).
+ */
 public record MuscleEngagementResponseDTO(
         Long id,
         String muscle,
