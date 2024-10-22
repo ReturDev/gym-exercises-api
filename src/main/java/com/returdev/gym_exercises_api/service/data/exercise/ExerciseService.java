48b7c240c02680e68d2de53ae77be02e7d64a594
ReturDev <sergiomm50@gmail.com>
2024-10-22 13:54:14 +0200

    refactor: add validation to ExerciseService methods
    - Added @Validated annotation to the ExerciseService interface to enable validation.
    - Updated saveExercise method to require @Valid annotation for the ExerciseEntity parameter, ensuring validation is applied during saving.
    - Changed updateExercise method to include @Valid annotation for the ExerciseEntity parameter, enforcing validation during updates.
    - Updated partialUpdateExercise method to require @Valid annotation for the ExerciseEntity parameter, ensuring validation during partial updates.

--- src/main/java/com/returdev/gym_exercises_api/service/data/exercise/ExerciseService.java
+++ src/main/java/com/returdev/gym_exercises_api/service/data/exercise/ExerciseService.java
@@ -1,22 +1,25 @@
 package com.returdev.gym_exercises_api.service.data.exercise;
 
 import com.returdev.gym_exercises_api.model.entities.ExerciseEntity;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Optional;
 
+@Validated
 public interface ExerciseService {
 
     Page<ExerciseEntity> getAllExercises(Pageable pageable);
 
     Optional<ExerciseEntity> getExerciseById(Long id);
 
-    ExerciseEntity saveExercise(ExerciseEntity exercise);
+    ExerciseEntity saveExercise(@Valid ExerciseEntity exercise);
 
-    ExerciseEntity updateExercise(ExerciseEntity exercise);
+    ExerciseEntity updateExercise(@Valid ExerciseEntity exercise);
 
-    ExerciseEntity partialUpdateExercise(ExerciseEntity patchExercise);
+    ExerciseEntity partialUpdateExercise(@Valid ExerciseEntity patchExercise);
 
     void deleteExercise(Long id);
 
