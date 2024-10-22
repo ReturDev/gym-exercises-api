1a197025e9c131aaa662df345523d345dc4c3179
ReturDev <sergiomm50@gmail.com>
2024-10-22 14:00:27 +0200

    refactor: update EntityDtoMapper to use PaginationResponseDTO
    - Changed method signatures to return PaginationResponseDTO for paginated responses.
    - Updated equipmentEntityToContentResponse and exerciseEntityToContentResponse methods to return a list of DTOs with pagination info.

--- src/main/java/com/returdev/gym_exercises_api/mappers/EntityDtoMapper.java
+++ src/main/java/com/returdev/gym_exercises_api/mappers/EntityDtoMapper.java
@@ -3,25 +3,22 @@ package com.returdev.gym_exercises_api.mappers;
 import com.returdev.gym_exercises_api.dto.request.EquipmentRequestDTO;
 import com.returdev.gym_exercises_api.dto.request.ExerciseRequestDTO;
 import com.returdev.gym_exercises_api.dto.request.pagination.PaginationRequestDTO;
-import com.returdev.gym_exercises_api.dto.response.ContentResponseDTO;
-import com.returdev.gym_exercises_api.dto.response.EquipmentResponseDTO;
-import com.returdev.gym_exercises_api.dto.response.ExerciseResponseDTO;
-import com.returdev.gym_exercises_api.dto.response.TokenResponseDTO;
+import com.returdev.gym_exercises_api.dto.response.*;
+import com.returdev.gym_exercises_api.dto.response.wrapper.ContentResponseDTO;
+import com.returdev.gym_exercises_api.dto.response.wrapper.PaginationResponseDTO;
 import com.returdev.gym_exercises_api.model.auth.AuthToken;
 import com.returdev.gym_exercises_api.model.entities.EquipmentEntity;
 import com.returdev.gym_exercises_api.model.entities.ExerciseEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface EntityDtoMapper {
-    
-    ContentResponseDTO<List<EquipmentResponseDTO>> equipmentEntityToContentResponse(Page<EquipmentEntity> equipmentEntityPage);
+
+    PaginationResponseDTO<EquipmentResponseDTO> equipmentEntityToContentResponse(Page<EquipmentEntity> equipmentEntityPage);
     ContentResponseDTO<EquipmentResponseDTO> equipmentEntityToContentResponse(EquipmentEntity equipmentEntity);
     EquipmentEntity equipmentRequestDtoToEntity(EquipmentRequestDTO equipmentRequestDTO);
 
-    ContentResponseDTO<List<ExerciseResponseDTO>> exerciseEntityToContentResponse(Page<ExerciseEntity> exerciseEntityPage);
+    PaginationResponseDTO<ExerciseResponseDTO> exerciseEntityToContentResponse(Page<ExerciseEntity> exerciseEntityPage);
     ContentResponseDTO<ExerciseResponseDTO> exerciseEntityToContentResponse(ExerciseEntity exerciseEntity);
     ExerciseEntity exerciseRequestDtoToEntity(ExerciseRequestDTO exerciseRequestDTO);
 
