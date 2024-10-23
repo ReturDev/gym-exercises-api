be7c22bb1b82c0b958646f68a5bb15a7a19681b9
ReturDev <sergiomm50@gmail.com>
2024-10-23 21:47:09 +0200

    feat: add custom annotation for 204 No Content responses
    - Introduced `NoContentResponseCode` annotation to document methods returning a 204 status in OpenAPI specifications via Swagger.
    - This annotation indicates that the request was successfully processed, but there is no content to return in the response body.
    - Includes properties for response code and description, enhancing API documentation clarity.

---
+++ src/main/java/com/returdev/gym_exercises_api/annotation/swagger/NoContentResponseCode.java
@@ -0,0 +1,46 @@
+package com.returdev.gym_exercises_api.annotation.swagger;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Custom annotation to document a method that returns a 204 No Content response
+ * in an OpenAPI specification using Swagger.
+ * <p>
+ * This annotation indicates that the API endpoint has successfully processed the request
+ * but does not have any content to return in the response body.
+ * </p>
+ * <p>
+ * The annotation includes the following properties:
+ * <ul>
+ *   <li><strong>responseCode:</strong> The HTTP status code "204", indicating that the request was successful but there is no content to send.</li>
+ *   <li><strong>description:</strong> A brief description stating "No Content".</li>
+ * </ul>
+ * </p>
+ * <p>
+ * This annotation is processed at runtime and is primarily used for generating API documentation
+ * with OpenAPI via Swagger.
+ * </p>
+ * <p>
+ * <strong>Example usage:</strong>
+ * <pre>{@code
+ * @NoContentResponseCode
+ * public ResponseEntity<Void> deleteItem(Long id) {
+ *     itemService.deleteItem(id);
+ *     return ResponseEntity.noContent().build();
+ * }
+ * }</pre>
+ * </p>
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(
+        responseCode = "204",
+        description = "No Content: The request was successful but there is no content to return."
+)
+public @interface NoContentResponseCode {}
+
