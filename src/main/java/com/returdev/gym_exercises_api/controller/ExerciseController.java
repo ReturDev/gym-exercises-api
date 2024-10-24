c68a852dd9b8a8716bf7231675694547424e7a93
ReturDev <sergiomm50@gmail.com>
2024-10-25 01:15:37 +0200

    feat: enhance ExerciseController with OpenAPI documentation and response handling
    - Added OpenAPI `@Operation` annotations to document the exercise-related endpoints.
    - Included response code annotations like `@OkResponseCode`, `@NotFoundResponseCode`, `@ConflictResponseCode`, and others for clearer API documentation.
    - Added custom descriptions for creating exercises, including valid muscle groups and activation levels.
    - Refactored `getExercises`, `getExerciseById`, `saveExercise`, `patchExercise`, and `deleteExercise` methods to include enhanced OpenAPI documentation.
    - Modified request and response handling with standardized error schemas and pagination support.

--- src/main/java/com/returdev/gym_exercises_api/controller/ExerciseController.java
+++ src/main/java/com/returdev/gym_exercises_api/controller/ExerciseController.java
@@ -1,5 +1,7 @@
 package com.returdev.gym_exercises_api.controller;
 
+import com.returdev.gym_exercises_api.annotation.swagger.request.EquipmentPaginationRequestParameters;
+import com.returdev.gym_exercises_api.annotation.swagger.response.*;
 import com.returdev.gym_exercises_api.dto.request.ExerciseRequestDTO;
 import com.returdev.gym_exercises_api.dto.request.pagination.ExercisePaginationRequestDTO;
 import com.returdev.gym_exercises_api.dto.response.ExerciseResponseDTO;
@@ -8,6 +10,11 @@ import com.returdev.gym_exercises_api.dto.response.wrapper.PaginationResponseDTO
 import com.returdev.gym_exercises_api.mappers.EntityDtoMapper;
 import com.returdev.gym_exercises_api.model.entities.ExerciseEntity;
 import com.returdev.gym_exercises_api.service.data.exercise.ExerciseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -29,6 +36,10 @@ import java.net.URI;
 @RequestMapping("v1/exercise")
 @RequiredArgsConstructor
 @PreAuthorize("isAuthenticated()")
+@Tag(name = "Exercise", description = "Endpoint to get exercises")
+@InternalServerErrorResponseCode
+@UnauthorizedResponseCode
+@BadRequestResponseCode
 public class ExerciseController {
 
     private static final String EXERCISE_RESOURCE_PATH = "v1/exercise";
@@ -42,8 +53,17 @@ public class ExerciseController {
      * @param pagination The pagination parameters.
      * @return A response entity containing a paginated list of exercises.
      */
+    @Operation(
+            summary = "Retrieve paginated list of exercises",
+            description = "Fetches a paginated list of exercises entities from the system. This endpoint requires a **public key** for access."
+    )
+    @OkResponseCode
+    @EquipmentPaginationRequestParameters
     @GetMapping()
-    public ResponseEntity<PaginationResponseDTO<ExerciseResponseDTO>> getExercises(@Valid ExercisePaginationRequestDTO pagination) {
+    public ResponseEntity<PaginationResponseDTO<ExerciseResponseDTO>> getExercises(
+            @Parameter(hidden = true)
+            @Valid ExercisePaginationRequestDTO pagination
+    ) {
         // Fetch paginated exercise entities from the service
         Page<ExerciseEntity> page = exerciseService.getAllExercises(
                 mapper.paginationRequestDtoToPageable(pagination)
@@ -61,6 +81,12 @@ public class ExerciseController {
      * @param id The ID of the exercise to retrieve.
      * @return A response entity containing the exercise details.
      */
+    @Operation(
+            summary = "Retrieve an exercises by ID",
+            description = "Fetches the exercises entity with the specified ID. This endpoint requires a **public key** for access."
+    )
+    @OkResponseCode
+    @NotFoundResponseCode
     @GetMapping("/{id}")
     public ResponseEntity<ContentResponseDTO<ExerciseResponseDTO>> getExerciseById(
             @PathVariable("id") Long id
@@ -79,6 +105,33 @@ public class ExerciseController {
      * @param exerciseRequestDTO The exercise data to create.
      * @return A response entity containing the created exercise details.
      */
+    @Operation(
+            summary = "Create a new exercise",
+            description = """
+        Creates a new exercise entity in the system. This endpoint requires a **private key** for access.
+        
+        **Valid Muscles**:
+        - UPPER_CHEST, LOWER_CHEST, INNER_CHEST
+        - DORSALS, WIDE_BACK, RHOMBOIDS, TRAPEZES, LUMBAR
+        - ANTERIOR_DELTOID, LATERAL_DELTOID, POSTERIOR_DELTOID
+        - QUADRICEPS, HAMSTRINGS, CALVES, BUTTOCKS
+        - BICEPS, TRICEPS, FOREARMS
+        - ABS, OBLIQUES
+        
+        **Valid Muscle Activation Levels**:
+        - HIGH, MEDIUM, LOW
+        """
+    )
+    @OkResponseCode
+    @NotFoundResponseCode
+    @ConflictResponseCode
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    schema = @Schema(
+                            ref = "#/components/schemas/SaveExercise"
+                    )
+            )
+    )
     @PostMapping()
     @PreAuthorize("hasRole('DEVELOPER')")
     public ResponseEntity<ContentResponseDTO<ExerciseResponseDTO>> saveExercise(
@@ -102,6 +155,33 @@ public class ExerciseController {
      * @param exerciseRequestDTO The updated exercise data.
      * @return A response entity containing the updated exercise details.
      */
+    @Operation(
+            summary = "Update an exercise",
+            description = """
+        Updates an existing exercise entity in the system. This endpoint requires a **private key** for access.
+        
+        **Valid Muscles**:
+        - UPPER_CHEST, LOWER_CHEST, INNER_CHEST
+        - DORSALS, WIDE_BACK, RHOMBOIDS, TRAPEZES, LUMBAR
+        - ANTERIOR_DELTOID, LATERAL_DELTOID, POSTERIOR_DELTOID
+        - QUADRICEPS, HAMSTRINGS, CALVES, BUTTOCKS
+        - BICEPS, TRICEPS, FOREARMS
+        - ABS, OBLIQUES
+        
+        **Valid Muscle Activation Levels**:
+        - HIGH, MEDIUM, LOW
+        """
+    )
+    @OkResponseCode
+    @NotFoundResponseCode
+    @ConflictResponseCode
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    schema = @Schema(
+                            ref = "#/components/schemas/UpdateExercise"
+                    )
+            )
+    )
     @PutMapping()
     @PreAuthorize("hasRole('DEVELOPER')")
     public ResponseEntity<ContentResponseDTO<ExerciseResponseDTO>> updateExercise(
@@ -125,6 +205,33 @@ public class ExerciseController {
      * @param exerciseRequestDTO The exercise data to update.
      * @return A response entity containing the updated exercise details.
      */
+    @Operation(
+            summary = "Partially update an exercise",
+            description = """
+        Partially updates an existing exercise entity in the system. This endpoint requires a **private key** for access.
+        
+        **Valid Muscles**:
+        - UPPER_CHEST, LOWER_CHEST, INNER_CHEST
+        - DORSALS, WIDE_BACK, RHOMBOIDS, TRAPEZES, LUMBAR
+        - ANTERIOR_DELTOID, LATERAL_DELTOID, POSTERIOR_DELTOID
+        - QUADRICEPS, HAMSTRINGS, CALVES, BUTTOCKS
+        - BICEPS, TRICEPS, FOREARMS
+        - ABS, OBLIQUES
+        
+        **Valid Muscle Activation Levels**:
+        - HIGH, MEDIUM, LOW
+        """
+    )
+    @OkResponseCode
+    @NotFoundResponseCode
+    @ConflictResponseCode
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    schema = @Schema(
+                            ref = "#/components/schemas/PatchExercise"
+                    )
+            )
+    )
     @PatchMapping()
     @PreAuthorize("hasRole('DEVELOPER')")
     public ResponseEntity<ContentResponseDTO<ExerciseResponseDTO>> patchExercise(
@@ -142,12 +249,19 @@ public class ExerciseController {
                 );
     }
 
+
     /**
      * Deletes an exercise by its ID.
      *
      * @param id The ID of the exercise to delete.
      * @return A response entity with no content.
      */
+    @Operation(
+            summary = "Delete an exercise",
+            description = "Removes the specified exercise entity from the system. This endpoint requires a **private key** for access."
+    )
+    @NotFoundResponseCode
+    @NoContentResponseCode
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('DEVELOPER')")
     public ResponseEntity<Void> deleteExercise(
