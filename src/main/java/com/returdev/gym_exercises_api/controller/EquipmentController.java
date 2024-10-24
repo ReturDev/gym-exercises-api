99003984b461f179c4f62c9036c6c62c1ccf4801
ReturDev <sergiomm50@gmail.com>
2024-10-24 14:02:31 +0200

    feat: enhance EquipmentController with Swagger documentation and response codes
    - Added `@Tag` annotation to classify the `EquipmentController` under "Equipment" for improved organization in API documentation.
    - Introduced response annotations for better documentation of potential responses, including `@InternalServerErrorResponseCode`, `@UnauthorizedResponseCode`, and `@BadRequestResponseCode`.
    - Enhanced the `getEquipments` method with `@Operation` annotation to provide a summary and description, including the requirement of a public key for access.
    - Added `@OkResponseCode` and `@EquipmentPaginationRequestParameters` annotations to the `getEquipments` method for standardizing responses and parameter documentation.
    - Enhanced `getEquipmentById` method with `@Operation`, `@OkResponseCode`, and `@NotFoundResponseCode` annotations for clear documentation of the endpoint behavior and access requirements.
    - Added `@Operation`, `@OkResponseCode`, `@NotFoundResponseCode`, and `@ConflictResponseCode` annotations to the save and update methods for equipment, detailing their purposes and access requirements.
    - Included request body specifications using `@io.swagger.v3.oas.annotations.parameters.RequestBody` for the save and update methods to clarify input expectations.
    - Enhanced `deleteEquipment` method with `@Operation`, `@NotFoundResponseCode`, and `@NoContentResponseCode` annotations to provide clear documentation of its behavior and access requirements.

--- src/main/java/com/returdev/gym_exercises_api/controller/EquipmentController.java
+++ src/main/java/com/returdev/gym_exercises_api/controller/EquipmentController.java
@@ -1,5 +1,7 @@
 package com.returdev.gym_exercises_api.controller;
 
+import com.returdev.gym_exercises_api.annotation.swagger.request.EquipmentPaginationRequestParameters;
+import com.returdev.gym_exercises_api.annotation.swagger.response.*;
 import com.returdev.gym_exercises_api.dto.request.EquipmentRequestDTO;
 import com.returdev.gym_exercises_api.dto.request.pagination.EquipmentPaginationRequestDTO;
 import com.returdev.gym_exercises_api.dto.response.EquipmentResponseDTO;
@@ -8,6 +10,11 @@ import com.returdev.gym_exercises_api.dto.response.wrapper.PaginationResponseDTO
 import com.returdev.gym_exercises_api.mappers.EntityDtoMapper;
 import com.returdev.gym_exercises_api.model.entities.EquipmentEntity;
 import com.returdev.gym_exercises_api.service.data.equipment.EquipmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -29,6 +36,10 @@ import java.net.URI;
 @RequestMapping("v1/equipment")
 @RequiredArgsConstructor
 @PreAuthorize("isAuthenticated()")
+@Tag(name = "Equipment", description = "Endpoint to get equipments")
+@InternalServerErrorResponseCode
+@UnauthorizedResponseCode
+@BadRequestResponseCode
 public class EquipmentController {
 
     private static final String EQUIPMENT_RESOURCE_PATH = "v1/equipment";
@@ -42,8 +53,15 @@ public class EquipmentController {
      * @param paginationRequestDTO The pagination parameters.
      * @return A response entity containing a paginated list of equipment.
      */
+    @Operation(
+            summary = "Retrieve paginated list of equipment",
+            description = "Fetches a paginated list of equipment entities from the system. This endpoint requires a **public key** for access."
+    )
+    @OkResponseCode
+    @EquipmentPaginationRequestParameters
     @GetMapping
     public ResponseEntity<PaginationResponseDTO<EquipmentResponseDTO>> getEquipments(
+            @Parameter(hidden = true)
             @Valid EquipmentPaginationRequestDTO paginationRequestDTO
     ) {
         // Fetch paginated equipment entities from the service
@@ -63,6 +81,12 @@ public class EquipmentController {
      * @param id The ID of the equipment to retrieve.
      * @return A response entity containing the equipment details.
      */
+    @Operation(
+            summary = "Retrieve an equipment by ID",
+            description = "Fetches the equipment entity with the specified ID from the system. This endpoint requires a **public key** for access."
+    )
+    @OkResponseCode
+    @NotFoundResponseCode
     @GetMapping("/{id}")
     public ResponseEntity<ContentResponseDTO<EquipmentResponseDTO>> getEquipmentById(
             @PathVariable("id") Long id
@@ -81,6 +105,20 @@ public class EquipmentController {
      * @param equipmentRequestDTO The equipment data to create.
      * @return A response entity containing the created equipment details.
      */
+    @Operation(
+            summary = "Save a new equipment",
+            description = "Creates a new equipment entity in the system. This endpoint requires a **private key** for access."
+    )
+    @OkResponseCode
+    @NotFoundResponseCode
+    @ConflictResponseCode
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    schema = @Schema(
+                            ref = "#/components/schemas/SaveEquipment"
+                    )
+            )
+    )
     @PostMapping()
     @PreAuthorize("hasRole('DEVELOPER')")
     public ResponseEntity<ContentResponseDTO<EquipmentResponseDTO>> saveEquipment(
@@ -107,6 +145,20 @@ public class EquipmentController {
      * @param equipmentRequestDTO The updated equipment data.
      * @return A response entity containing the updated equipment details.
      */
+    @Operation(
+            summary = "Update an equipment",
+            description = "Updates an existing equipment entity in the system. This endpoint requires a **private key** for access."
+    )
+    @OkResponseCode
+    @NotFoundResponseCode
+    @ConflictResponseCode
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    schema = @Schema(
+                            ref = "#/components/schemas/UpdateEquipment"
+                    )
+            )
+    )
     @PutMapping()
     @PreAuthorize("hasRole('DEVELOPER')")
     public ResponseEntity<ContentResponseDTO<EquipmentResponseDTO>> updateEquipment(
@@ -134,6 +186,12 @@ public class EquipmentController {
      * @param id The ID of the equipment to delete.
      * @return A response entity with no content.
      */
+    @Operation(
+            summary = "Delete an equipment",
+            description = "Removes the specified equipment entity from the system. This endpoint requires a **private key** for access."
+    )
+    @NotFoundResponseCode
+    @NoContentResponseCode
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('DEVELOPER')")
     public ResponseEntity<Void> deleteEquipment(
